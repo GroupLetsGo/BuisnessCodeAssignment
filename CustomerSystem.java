06e803c319439b4d8a0ad04623b21f22817db391
Yilia Wang <yanyinwang3@gmail.com>
2021-04-08 09:48:25 -0400

    enter user info 1

--- CustomerSystem.java
+++ CustomerSystem.java
@@ -30,7 +30,7 @@ class CustomerSystem{
             if (userInput.equals(enterCustomerOption)){
                 // Only the line below may be editted based on the parameter list and how you design the method return
 		        // Any necessary variables may be added to this if section, but nowhere else in the code
-                enterCustomerInfo();
+                String[] customerInfo = enterCustomerInfo(reader,validPostalCodes);
             }
             else if (userInput.equals(generateCustomerOption)) {
                 // Only the line below may be editted based on the parameter list and how you design the method return
